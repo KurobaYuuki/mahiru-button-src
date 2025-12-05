6035e41c6588998f46a7f706a5d068d13b0c07c8
KurobaYuuki <78408086+KurobaYuuki@users.noreply.github.com>
2025-12-05 23:38:45 +0800

    Compatible up to Node.js v22

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,12 @@ module.exports = {
       }),
     ],
   },
+  chainWebpack: config => {
+    // See: https://github.com/vuejs/vue-cli/issues/5753
+    // Prevents "Multiple assets emit different content to the same filename index.html"
+    config.plugin('copy').tap(args => {
+      args[0].patterns[0].globOptions.ignore.push('**/index.html');
+      return args;
+    });
+  }
 };
